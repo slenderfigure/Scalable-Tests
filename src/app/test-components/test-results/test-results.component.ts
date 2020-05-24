ba083234d76b05d02067d7bd56ecc87b91650e23
Adison Pena <hirayoki.nova@gmail.com>
2020-05-23 23:06:12 -0400

    M

--- src/app/test-components/test-results/test-results.component.ts
+++ src/app/test-components/test-results/test-results.component.ts
@@ -38,7 +38,7 @@ export class TestResultsComponent implements OnInit {
         value: this.wrong,
         icon: `<svg height="24" viewBox="0 0 24 24" width="24" fill="#fd4242"><path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/><path d="M0 0h24v24H0z" fill="none"/></svg>`
       },
-      { label: 'Your score', value: `${this.test.score} / ${this.test.points} - (${this.socorePercent}%)` },
+      { label: 'Your score', value: `${this.test.score} / ${this.test.points} - (${this.socorePercent}%)` }
     ];
   }
 
