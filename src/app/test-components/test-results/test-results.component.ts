9f0c0c29b32283e6fe0a4d6881bbf90c0616a7b8
Adison Pena <hirayoki.nova@gmail.com>
2020-05-23 20:02:14 -0400

    Added new test to tests.json

--- src/app/test-components/test-results/test-results.component.ts
+++ src/app/test-components/test-results/test-results.component.ts
@@ -38,7 +38,7 @@ export class TestResultsComponent implements OnInit {
         value: this.wrong,
         icon: `<svg height="24" viewBox="0 0 24 24" width="24" fill="#fd4242"><path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/><path d="M0 0h24v24H0z" fill="none"/></svg>`
       },
-      { label: 'Your score', value: `${this.test.score} (${this.socorePercent}%)` },
+      { label: 'Your score', value: `${this.test.score} / ${this.test.points} - (${this.socorePercent}%)` },
     ];
   }
 
@@ -72,3 +72,89 @@ export class TestResultsComponent implements OnInit {
     return (time >= 3600) ? `${formatted} hr(s)` : (time < 3600 && time >= 60) ? `${formatted} min(s)` : `${formatted} sec(s)`;
   }
 }
+
+const test: Test = new Test({
+  subject: 'Literature',
+  points: 25,
+  questions: [
+    {
+      id: 1,
+      section: "Section 1",
+      problem: "Choose the correct answer",
+      title: "Mark Twain wrote about which of these characters?",
+      points: 5,
+      answers: [
+        "Little Boy Blue",
+        "Red Riding Hood",
+        "Dennis the Menace",
+        "Yukon Territory"
+      ],
+      correctAnswer: ["Tom Sawyer"],
+      timeLimit: "00:00:15"
+    },
+    {
+      id: 2,
+      section: "Section 1",
+      problem: "Choose the correct answer",
+      title: `This character says, "Please, sir, I want some more." Who is it?`,
+      points: 5,
+      answers: [
+        "Atticus Finch",
+        "Darth Vader",
+        "Batman",
+        "Pluto"
+      ],
+      correctAnswer: ["Oliver Twist"],
+      timeLimit: "00:00:15"
+    },
+    {
+      id: 3,
+      section: "Section 1",
+      problem: "Choose the correct answer",
+      title: `Who was the main character in J.D. Salinger's "Catcher in the Rye?"`,
+      points: 5,
+      answers: [
+        "Bart Simpson",
+        "Huckleberry Finn",
+        "Clark Kent",
+        "Saitama" 
+      ],
+      correctAnswer: ["Holden Caulfield"],
+      timeLimit: "00:00:15"
+    },
+    {
+      id: 4,
+      section: "Section 1",
+      problem: "Choose the correct answer",
+      title: `Which is the "Scarlet Letter?"`,
+      points: 5,
+      answers: [
+        "X",
+        "Y",
+        "Z",
+        "Q"
+      ],
+      correctAnswer: ["A"],
+      timeLimit: "00:00:15"
+    },
+    {
+      id: 5,
+      section: "Section 1",
+      problem: "Choose the correct answer",
+      title: `Which person penned the Harry Potter novels?`,
+      points: 5,
+      answers: [
+        "Mark Twain",
+        "Oprah Winfrey",
+        "Dan Brown",
+        "Coyote Peterson"
+      ],
+      correctAnswer: ["J.K. Rowling"],
+      timeLimit: "00:00:15"
+    }
+  ],
+  timeLimit: 75,
+  difficulty: 'Low'
+});
+
+console.log(JSON.stringify(test));
--- src/app/test-components/test.model.ts
+++ src/app/test-components/test.model.ts
@@ -1,7 +1,7 @@
 import { Question } from './question.model';
 
 export class Test {
-  id: string | number;
+  id: string;
   subject: string;
   points: number;
   questions: Question[];
@@ -14,14 +14,13 @@ export class Test {
   sessionCompleted: boolean;
 
   constructor(params: {
-    id: string | number,
     subject: string,
     points: number,
     questions: Question[],
     timeLimit: number,
     difficulty: string
   }) {
-    this.id = params.id;
+    this.id = this.uniqueId;
     this.subject = params.subject;
     this.points = params.points;
     this.questions = params.questions;  
@@ -33,4 +32,18 @@ export class Test {
     this.sessionCompleted = false;
   }
   
+  private get uniqueId(): string {
+    let uniqueId = '';
+
+    for (let i = 15; i > 0; i--) {
+      const arr = [
+        Math.floor((Math.random() * (57 - 48)) + 48),
+        Math.floor((Math.random() * (90 - 65)) + 65),
+        Math.floor((Math.random() * (122 - 97)) + 97)
+      ];
+      uniqueId += `${String.fromCharCode(arr[Math.round(Math.random() * 2)])}`;
+    }
+    return uniqueId;
+  }
+
 }
