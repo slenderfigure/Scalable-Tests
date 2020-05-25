ee66741476c400d475527830ba8b189b002f32cf
Adison Pena <hirayoki.nova@gmail.com>
2020-05-25 16:10:14 -0400

    Removed stuff

--- src/app/test-components/test-results/answers-review/answers-review.component.ts
+++ src/app/test-components/test-results/answers-review/answers-review.component.ts
@@ -1,13 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { ViewChild, ElementRef } from '@angular/core';
 import { Question } from '../../question.model';
 import { Test } from '../../test.model';
+import { fromEvent } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-answers-review',
   templateUrl: './answers-review.component.html',
   styleUrls: ['./answers-review.component.css']
 })
-export class AnswersReviewComponent implements OnInit {
+export class AnswersReviewComponent implements OnInit, AfterViewInit {
+  @ViewChild('goUpButton') goUpButton: ElementRef<HTMLButtonElement>;
   questions: Question[];
 
   constructor() { }
@@ -17,4 +21,19 @@ export class AnswersReviewComponent implements OnInit {
     this.questions = test.questions;  
   }
 
+  ngAfterViewInit(): void {
+    fromEvent(window, 'scroll').subscribe(() => {
+      const button = this.goUpButton.nativeElement;
+
+      if (window.scrollY >= 700) {
+        button.classList.add('show');
+      } else {
+        button.classList.remove('show');
+      }
+    });
+  }
+
+  goToTop(): void {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
 }
