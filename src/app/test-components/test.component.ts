2d9271ac6c9dd498efc4938552740926a6e7983f
Adison Pena <hirayoki.nova@gmail.com>
2020-05-30 20:18:13 -0400

    M

--- src/app/test-components/test.component.ts
+++ src/app/test-components/test.component.ts
@@ -17,45 +17,6 @@ export class TestComponent implements OnInit {
 
   ngOnInit(): void {
     this.testList$ = this.ts.getAllTests();
-
-    const questions: Question[] = [
-      { 
-        id: 11,
-        type: 1,
-        section: 'Section 2',
-        problem: 'Choose the correct answer',
-        title: 'The balancing of light within a photograph is known as the:',
-        points: 2.3,
-        answers: [
-          'White balance',
-          'The aperture',
-          'The shutter speed',
-          'None of the above'         
-        ],
-        correctAnswer: ['The exposure'],
-        timeLimit: '00:00:20',
-        image: 'assets/img/test-img24.jpg'
-      },
-      { 
-        id: 12,
-        type: 1,
-        section: 'Section 2',
-        problem: 'Choose the correct answer',
-        title: 'Which is a larger aperture?',
-        points: 2.3,
-        answers: [
-          'f/2.8',
-          '1600',
-          '1/300',
-          'f/64'
-        ],
-        correctAnswer: ['f/1.4'],
-        timeLimit: '00:00:20',
-        image: 'assets/img/test-img25.jpg'
-      }
-    ];
-
-    // console.log(JSON.stringify(questions));
   }
 
 }
