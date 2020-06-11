0610845ab39f0e5d2f14bc80798f36adef257b80
Adison Pena <hirayoki.nova@gmail.com>
2020-06-11 13:06:42 -0400

    m

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main-page.component.css']
 })
 export class MainPageComponent implements OnInit {
+  name: string = 'Adison Peña';
 
   constructor() { }
 
