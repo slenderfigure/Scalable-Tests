551c280f99bc1e1ac13b3297e70b92fc5755e6f2
Adison Pena <hirayoki.nova@gmail.com>
2020-06-11 13:15:19 -0400

    m

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -6,11 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main-page.component.css']
 })
 export class MainPageComponent implements OnInit {
-  name: string = 'Adison Peña';
+  user: { name: string, age: number } = {
+    name: 'Adison Peña',
+    age: 26
+  };
 
   constructor() { }
 
   ngOnInit(): void {
     
+  }
+
+  onClick(): void {
+    alert(`Your age is ${this.user.age}`);
   }
 }
